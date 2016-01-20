aa00e5449f1eaf43caed4679839ea04cd37f091c
Gesly George <gesly.george@instinet.com>
2016-01-20 08:34:17 -0800

    updated version file & VS project files

--- src/hb-version.h
+++ src/hb-version.h
@@ -104,11 +104,11 @@
 HB_BEGIN_DECLS
 
 
-#define HB_VERSION_MAJOR 0
-#define HB_VERSION_MINOR 9
-#define HB_VERSION_MICRO 41
+#define HB_VERSION_MAJOR 1
+#define HB_VERSION_MINOR 1
+#define HB_VERSION_MICRO 3
 
-#define HB_VERSION_STRING "0.9.41"
+#define HB_VERSION_STRING "1.1.3"
 
 #define HB_VERSION_ATLEAST(major,minor,micro) \
 	((major)*10000+(minor)*100+(micro) <= \
